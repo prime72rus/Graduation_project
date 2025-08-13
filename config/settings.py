3db2853b6d9f8886b7f3a1c4ef6c09ca064f6f14
Dmitrii Grechko <prime.optimys@mail.ru>
2025-08-13 16:07:21 +0500

    Регистрация приложений ads, users в settings.py

--- config/settings.py
+++ config/settings.py
@@ -50,6 +50,9 @@ INSTALLED_APPS = [
     "drf_spectacular",
     "drf_spectacular_sidecar",
     "corsheaders",
+
+    "users",
+    "ads",
 ]
 
 MIDDLEWARE = [
@@ -164,7 +167,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1000),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
 

5d4bd8ef98752d30a95629b7bf86cac5dc2554f3
Dmitrii Grechko <prime.optimys@mail.ru>
2025-08-13 15:27:42 +0500

    Добавление приложений в проект Django feat: startapp users startapp ads

--- config/settings.py
+++ config/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-AUTH_USER_MODEL = "users.User"
+# AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/

b9c151bd30a8143f83c985299753a00e3c9d0c2f
Dmitrii Grechko <prime.optimys@mail.ru>
2025-08-15 09:36:09 +0500

    style: ads/model.py

--- ads/models.py
+++ ads/models.py
@@ -9,28 +9,25 @@ class Ad(models.Model):
     title = models.CharField(
         verbose_name="Название товара",
         max_length=255,
-        help_text="Укажите название товара"
+        help_text="Укажите название товара",
     )
     price = models.PositiveIntegerField(
-        verbose_name="Цена товара",
-        default=0,
-        help_text="Укажите цену товара"
+        verbose_name="Цена товара", default=0, help_text="Укажите цену товара"
     )
     description = models.TextField(
-        verbose_name="Описание товара",
-        help_text="Укажите описание товара"
+        verbose_name="Описание товара", help_text="Укажите описание товара"
     )
     author = models.ForeignKey(
         "users.User",
         verbose_name="Пользователь который создал объявление",
         on_delete=models.CASCADE,
         related_name="ads",
-        help_text="Укажите пользователя создавшего объявление"
+        help_text="Укажите пользователя создавшего объявление",
     )
     created_at = models.DateTimeField(
         verbose_name="Дата создания объявления",
         auto_now_add=True,
-        help_text="Укажите дату создания объявления"
+        help_text="Укажите дату создания объявления",
     )
 
     class Meta:
@@ -48,27 +45,26 @@ class Review(models.Model):
     """
 
     text = models.TextField(
-        verbose_name="Отзыв на товар",
-        help_text="Укажите текст отзыва"
+        verbose_name="Отзыв на товар", help_text="Укажите текст отзыва"
     )
     author = models.ForeignKey(
         "users.User",
         verbose_name="Автор отзыва",
         on_delete=models.CASCADE,
         related_name="reviews",
-        help_text="Укажите автора отзыва"
+        help_text="Укажите автора отзыва",
     )
     ad = models.ForeignKey(
         "ads.Ad",
         verbose_name="Объявление, под которым оставлен отзыв",
         on_delete=models.CASCADE,
         related_name="reviews",
-        help_text="Укажите объявление, под которым оставлен отзыв"
+        help_text="Укажите объявление, под которым оставлен отзыв",
     )
     created_at = models.DateTimeField(
         verbose_name="Дата создания отзыва",
         auto_now_add=True,
-        help_text="Укажите дату создания отзыва"
+        help_text="Укажите дату создания отзыва",
     )
 
     class Meta:
